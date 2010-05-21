7bfa69c8bb761154e6974d9b46f112157729618a
balqui <balqui@e097de5e-064c-4130-bf0a-dac29718e5f8>
2010-05-21 12:14:32 +0000

    now actually adding dataset.py and some datasets

---
+++ trunk/dataset.py
@@ -0,0 +1,71 @@
+"""
+
+Project: yacaree
+Programmer: JLB
+
+Description:
+
+transactional dataset, read in from a txt file
+one transaction per line
+items in each transaction separated by whitespace
+ as per string method split()
+whitespace characters as per split() cannot occur
+ inside the items
+empty transactions are ignored
+dataset replicated into two dicts:
+ transcns: set of items per transaction id (unsigned, starts at zero)
+ occurncs: set of transaction ids per item
+also:
+ scale (maybe to migrate somewhere else)
+ nrits
+ nrtr
+ nrocc
+ univ
+ dataset filename
+ datasetfile, closed, should one need to open it again
+
+"""
+
+from iface import iface
+from collections import defaultdict
+
+class Dataset:
+
+    def __init__(self,filename=None):
+        "find actual file, open, and read the dataset in"
+        iface.repong()
+        if filename is None:
+            iface.reportwarning("No dataset file specified.")
+            filename = raw_input("Dataset File Name? ")
+        if len(filename)<=3 or filename[-4] != '.':
+            filename +=".txt"
+        self.datasetfile = iface.openfile(filename)
+        self.filename = filename
+        iface.report("Reading in dataset from file " +
+                     filename + " and computing some parameters...")
+        self.nrocc = 0
+        self.nrtr = 0
+        self.univ = set([])
+        self.transcns = defaultdict(set)
+        self.occurncs = defaultdict(set)
+        for line in self.datasetfile:
+            iface.pong()
+            isempty = True
+            for el in line.strip().split():
+                if len(el)>0:
+                    isempty = False
+                    self.nrocc += 1
+                    self.univ.add(el)
+                    self.transcns[self.nrtr].add(el)
+                    self.occurncs[el].add(self.nrtr)
+            if not isempty:
+                self.nrtr += 1
+        self.nrits = len(self.univ)
+        iface.say("... dataset read in. Consists of " +
+                     str(self.nrtr) + " transactions from among " +
+                     str(self.nrits) + " items, with a total of " +
+                     str(self.nrocc) + " item occurrences.")
+        iface.endreport()
+
+
+
