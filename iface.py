f1c1af940473b83bae9b9ca456816e8428e96ced
balqui <balqui@e097de5e-064c-4130-bf0a-dac29718e5f8>
2010-05-21 12:13:30 +0000

    rewrote static and iface, added dataset

--- iface.py
+++ iface.py
@@ -3,43 +3,101 @@
 Project: yacaree
 Programmer: JLB
 
-Description: vt100-style interface, to be replaced by a reasonable GUI some day
+Description:
 
-verb: verbosity level (0:almost silent, 1:progress rep, 2:further info, 3:a lot)
+Interface 'static'-like class for provisional running of the
+ tool, vt100-style - lowercase initial due to static use
+To encapsulate all "print" statements so that migration
+ to Python 3 is easier
+And to become a GUI some day
+
+verb: verbosity level - I THINK IT DOES NOT WORK YET
+ 0:almost silent
+ 1:progress rep
+ 2:further info
+ 3:a lot
 pongs: a dot is written every that many pong calls
+openfile: checks readability
 
 Usage:
-say: outputs a string message, no line breaks, may add a verb level clearance
-     to allow blocking it, default is only say it at verb level 3
-report: likewise, prepends a line break
+say: outputs a string message, no line breaks,
+ may add a verb level clearance to allow blocking it,
+ default is only say it at verb level 3
+report: likewise, prepends a line break,
+ variants for warnings and errors
 pong ("dual to ping"): acts as progress bar advance
 repong: initializes it and admits a "speed" adjustment
 
 """
 
-verb = 1
+import sys
 
-pongs = 250
+class iface:
 
-countpongs = 0
-
-def repong(pn=0):
+    verb = 3
+    pongs = 250
     countpongs = 0
-    if pn > 0:
-        pongs = pn
+    pendinglinebreak = False
 
-def pong():
-    countpongs += 1
-    if countpongs == pongs:
-        say(".",1)
-        countpongs = 0
+    @classmethod
+    def repong(cls,pn=0):
+        cls.countpongs = 0
+        if pn > 0:
+            cls.pongs = pn
 
-def say(m,vb=3):
-    if verb >= vb:
-        print m,
+    @classmethod
+    def pong(cls):
+        cls.countpongs += 1
+        if cls.countpongs == cls.pongs:
+            cls.say(".",1)
+            cls.countpongs = 0
 
-def report(m="",vb=3):
-    "flush previous messages and write a starting message"
-    if verb >= vb:
-        print
-        print m,
+    @classmethod
+    def say(cls,m,vb=3):
+        if cls.verb >= vb:
+            print m,
+            cls.pendinglinebreak = True
+
+    @classmethod
+    def report(cls,m="",vb=3):
+        "flush previous messages, write a starting message"
+        if cls.verb >= vb:
+            if cls.pendinglinebreak: 
+                print
+            print "[yacaree]", m,
+            cls.pendinglinebreak = True
+
+    @classmethod
+    def endreport(cls):
+        "flush"
+        if cls.pendinglinebreak: 
+            print
+        cls.pendinglinebreak = False
+
+    @classmethod
+    def reportwarning(cls,m="",vb=1):
+        "flush, write warning message at low verbosity"
+        if cls.verb >= vb:
+            print
+            print "[yacaree warning]", m
+
+    @classmethod
+    def reporterror(cls,m="",vb=0):
+        "flush, write verbosity-independent error message, exit"
+        m = "[yacaree error] "+m
+        if cls.pendinglinebreak: 
+            print
+        print m
+        sys.exit(m)
+
+    @classmethod
+    def openfile(cls,filename):
+        cls.report("Attempting to read dataset from file "+filename)
+        try:
+            f = open(filename)
+            f.readline()
+            f.close
+            return open(filename)
+        except (IOError, OSError):
+            cls.reporterror("nonexistent or unreadable file")
+
