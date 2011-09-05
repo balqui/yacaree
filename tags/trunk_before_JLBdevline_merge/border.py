f660304f56786630bbfdb290a0a09ff90f7e41c5
balqui <balqui@e097de5e-064c-4130-bf0a-dac29718e5f8>
2011-09-05 11:49:29 +0000

    saving current trunk before evolving into iPred

---
+++ tags/trunk_before_JLBdevline_merge/border.py
@@ -0,0 +1,74 @@
+class Border:
+    """
+    Implements the border algorithm to construct closure lattices
+    """
+
+    def __init__(self):
+        "list of sets, initially empty"
+        self.contents = []
+
+    def append(self,e):
+        "should subclass list when upgrading to Python 3"
+        self.contents.append(e)
+
+    def cover_update(self,e,latt):
+        """
+        return all covers of e and take them out of the border
+        distinguish candidates that were already in the border
+        lattice needed to get intersection as a closure
+        """
+        candidates_in = []
+        candidates_out = set([])
+        cover = []
+        newborder = []
+        for ee in self.contents:
+            if ee <= e:
+                "intersection is border element, potential cover"
+                candidates_in.append(ee)
+            else:
+                "potential cover is proper subset intersection"
+                rr = set(ee.intersection(e))
+                r = latt.close(rr)
+                candidates_out.add(r)
+                newborder.append(ee)
+        for ee in candidates_in:
+            "pick maxima, return the others back to the border"
+            for eee in list(candidates_out) + candidates_in:
+                if ee < eee:
+                    "is this really possible? I think I proved NOT"
+                    newborder.append(ee)
+                    break
+            else:
+                "no larger candidate"
+                cover.append(ee)
+        for ee in candidates_out:
+            "pick maxima, discard the others"
+            for eee in list(candidates_out) + candidates_in:
+                if ee < eee:
+                    break
+            else:
+                "no larger candidate"
+                cover.append(ee)
+        self.contents = newborder
+        return cover
+
+if __name__ == "__main__":
+
+    class pseudolatt():
+        "stub to test Border with plain intersections"
+        def __init__(self):
+            pass
+        def close(self,a):
+            return frozenset(a)
+
+    c = pseudolatt()
+    
+    b = Border()
+    b.append(set(['a']))
+    b.append(set(['b','c']))
+    b.append(set(['c','d']))
+    b.append(set(['d','e']))
+    print b.contents
+    print b.cover_update(set(['a','b','c']),c)
+    print b.contents
+    
