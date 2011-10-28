468284e1ba7b787d914672a04689acdb3b5a32b1
balqui <balqui@e097de5e-064c-4130-bf0a-dac29718e5f8>
2011-10-28 17:39:34 +0000

    adding choose-iface, ToDo, flheap, and yaflheap

---
+++ trunk/choose_iface.py
@@ -0,0 +1,12 @@
+"""
+to choose text interface or GUI
+
+uncomment desired line
+
+it does not work smoothly yet, to run by double-clicking one must use the GUI
+"""
+
+##from iface_TEXT import * ## for text output through Python console
+
+from iface import * ## for the Tk GUI, only for launching yacaree.py
+
