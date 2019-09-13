320840c1e003264e8061c3e0414d430cbc51c193
balqui <jose.luis.balcazar@upc.edu>
2019-09-13 19:50:35 +0200

    flip iface to text for now

--- choose_iface.py
+++ choose_iface.py
@@ -6,7 +6,7 @@ uncomment desired line
 it does not work smoothly yet, to run by double-clicking one must use the GUI
 """
 
-##from iface_TEXT import * ## for text output through Python console
+from iface_TEXT import * ## for text output through Python console
 
-from iface import * ## for the Tk GUI, only for launching yacaree.py
+##from iface import * ## for the Tk GUI, only for launching yacaree.py
 
