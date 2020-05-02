57c464bcab2ea1e04dce56e1af611f3adacec696
balqui <jose.luis.balcazar@upc.edu>
2020-05-02 16:41:59 +0200

    File choose_iface.py should be unnecessary and redundant from now on!

--- choose_iface.py
+++ choose_iface.py
@@ -1,5 +1,16 @@
 """
-to choose text interface or GUI
+THIS FILE SHOULD BE INNECESSARY BY NOW AND IS REMOVED FROM VERSION CONTROL
+
+It was employed to choose text interface or GUI up to around v1.2.2.
+By now the way to choose the GUI is calling with the flag -g upon
+launching python yacaree.py (whether Python 2 or Python 3, depends
+on the version you are looking at).
+
+It remains a bit unclear at this point how to handle this on Windows
+as I no longer have such a machine. Formerly, there was a way to 
+launch the GUI by double-clicking on the yacaree.py file.
+
+Earlier info:
 
 uncomment desired line - as of today, only GUI works,
 plan is to add iface to the statics and also to move up to argparse
