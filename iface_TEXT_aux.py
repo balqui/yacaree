061e0b976d98603c60731e414b12150ec7169022
balqui <jose.luis.balcazar@upc.edu>
2020-05-10 19:07:33 +0200

    further progress on interfaces

--- iface_TEXT_aux.py
+++ iface_TEXT_aux.py
@@ -96,8 +96,9 @@ class iface_text:
         m = m + " Exiting.\n"
         if statics.logfile: statics.logfile.write(str(datetime.now()) + " " + m)
         exit("[yacaree error] " + m)
-
-    def ask_input(cls,prompt):
+    
+    @staticmethod
+    def ask_input(prompt):
         "See import from six.moves above"
         if statics.logfile: statics.logfile.write("Asked:" + prompt + "\n")
         ans = raw_input(prompt)
@@ -157,10 +158,13 @@ class iface_gui:
     "Initially a case of something like a singleton pattern, not anymore"
 
     def __init__(self):
+        """Instantiation is made before having a yacaree to  
+        bind the buttons to, hence everything deferred to self.go()
+        """
         pass
 
-    def go(self, mainprog):
-        "Parts of this must go into a regular __init__()"
+    def go(self, yacaree):
+        "Bindings could not be made in a regular __init__()"
         self.root = Tkinter.Tk()
 
         button_width = 35
@@ -217,7 +221,7 @@ class iface_gui:
                           width = button_width,
                           height = button_height,
                           state = Tkinter.DISABLED,
-                          command = mainprog.standard_run_all)
+                          command = yacaree.standard_run_all)
         self.run.pack()
 
         self.run50 = Tkinter.Button(process_frame)
@@ -225,7 +229,7 @@ class iface_gui:
                           width = button_width,
                           height = button_height,
                           state = Tkinter.DISABLED,
-                          command = mainprog.standard_run)
+                          command = yacaree.standard_run)
         self.run50.pack()
 
         self.finish_button = Tkinter.Button(process_frame)
@@ -254,127 +258,115 @@ class iface_gui:
         self.run50.configure(state = Tkinter.NORMAL)
         self.report("Selected dataset in file " + statics.filenamefull + ".")
 
-    def finish(cls):
+    def finish(self):
         if statics.logfile: statics.logfile = None # not sure still necessary
-        cls.console.bell()
-        cls.root.destroy()
+        self.sound_bell()
+        self.root.destroy()
         exit(0)
 
-    def enable_again(cls):
+    def enable_again(self):
         "After one of run/run50, user may wish to run the other"
-        # ~ cls.run.configure(state = Tkinter.DISABLED)
-        # ~ cls.run50.configure(state = Tkinter.DISABLED)
-        cls.run.configure(state = Tkinter.NORMAL)
-        cls.run50.configure(state = Tkinter.NORMAL)
-        cls.filepick.configure(state = Tkinter.NORMAL)
+        self.run.configure(state = Tkinter.NORMAL)
+        self.run50.configure(state = Tkinter.NORMAL)
+        self.filepick.configure(state = Tkinter.NORMAL)
 
-    def disable_again(cls):
+    def disable_again(self):
         pass
 
-    def enable_finish(cls):
-        cls.finish_button.configure(state = Tkinter.NORMAL)
+    def enable_finish(self):
+        self.finish_button.configure(state = Tkinter.NORMAL)
 
-    def disable_finish(cls):
-        cls.finish_button.configure(state = Tkinter.DISABLED)
-##        cls.finish_button.update()
+    def disable_finish(self):
+        self.finish_button.configure(state = Tkinter.DISABLED)
 
-    def disable_filepick(cls):
-        cls.filepick.configure(state = Tkinter.DISABLED)
+    def disable_filepick(self):
+        self.filepick.configure(state = Tkinter.DISABLED)
 
-    def disable_run(cls):
-        cls.run.configure(state = Tkinter.DISABLED)
-        cls.run50.configure(state = Tkinter.DISABLED)
+    def disable_run(self):
+        self.run.configure(state = Tkinter.DISABLED)
+        self.run50.configure(state = Tkinter.DISABLED)
 
-    def report(cls,m):
-        cls.clock_at_report = clock()
+    def report(self, m):
+        self.clock_at_report = clock()
         m = " " + m + "\n"
-        cls.console.insert(Tkinter.END,"[yacaree]" + m)
-        cls.console.see("end-2c")
-        cls.console.update()
+        self.console.insert(Tkinter.END,"[yacaree]" + m)
+        self.console.see("end-2c")
+        self.console.update()
         if statics.logfile: 
             statics.logfile.write(str(datetime.now()) + m)
 
-    def possibly_report(cls,m):
+    def possibly_report(self, m):
         "report only if too long time elapsed since last reporting"
         clock_now = clock()
-        if clock_now - cls.clock_at_report > statics.report_period:
-            cls.report(m)
+        if clock_now - self.clock_at_report > statics.report_period:
+            self.report(m)
 
-    def endreport(cls):
+    def endreport(self):
         pass
 
-    def reportwarning(cls,m):
-        cls.clock_at_report = clock()
+    def reportwarning(self, m):
+        self.clock_at_report = clock()
         m = " " + m + "\n"
-        cls.console.insert(Tkinter.END,"[yacaree warning]" + m)
-        cls.console.see("end-2c")
-        cls.console.update()
+        self.console.insert(Tkinter.END,"[yacaree warning]" + m)
+        self.console.see("end-2c")
+        self.console.update()
         if statics.logfile: 
             statics.logfile.write(str(datetime.now()) + m)
 
-    def reporterror(cls,m):
-        cls.clock_at_report = clock()
+    def reporterror(self, m):
+        self.clock_at_report = clock()
         m += "\n"
-        cls.console.insert(Tkinter.END,"[yacaree error] " + m)
+        self.console.insert(Tkinter.END,"[yacaree error] " + m)
         m = "Error: " + m
         if statics.logfile: statics.logfile.write(str(datetime.now()) + " " + m)
         sleep(10)
-        cls.root.destroy()
+        self.sound_bell()
+        self.root.destroy()
         exit(m)
 
-    def report_log_file(cls, filename):
-        cls.clock_at_report = clock()
+    def report_log_file(self, filename):
+        self.clock_at_report = clock()
         m_log = " Log file " + filename + ".log (this file) set up.\n"
         m_cls = " Log file " + filename + ".log set up.\n"
-        cls.console.insert(Tkinter.END,"[yacaree]" + m_cls)
-        cls.console.see("end-2c")
-        cls.console.update()
+        self.console.insert(Tkinter.END,"[yacaree]" + m_cls)
+        self.console.see("end-2c")
+        self.console.update()
         if statics.logfile: 
             statics.logfile.write(str(datetime.now()) + m_log)
 
-        # ~ "not reported like the rest for lack of log file"
-        # ~ if statics.logfile: 
-            # ~ "print already done, here just log entry"
-            # ~ statics.logfile.write(str(datetime.now()) + " " + m + "\n")
-            # ~ stdout.flush()
-
-    def storefilename(cls, filename):
+    def storefilename(self, filename):
         if len(filename)<=3 or filename[-4] != '.':
             statics.filename = filename
             statics.filenamefull = filename + statics.filenamext
         else:
-            '''statics.filenamext should not be modified:
-            o/w if a new dataset is loaded, the default extension no longer applies
-            '''
-            # ~ statics.filename, statics.filenamext = filename.rsplit('.',1)
             statics.filename, _ = filename.rsplit('.',1)
             statics.filenamefull = filename
 
-    def openfile(cls,filename,mode="r"):
+    def openfile(self, filename,mode = "r"):
         if mode == "r":
-            cls.report("Opening file " +
+            self.report("Opening file " +
                        filename + " for reading.")
             try:
                 f = open(filename)
                 f.readline()
                 f.close
-                cls.report("File is now open.")
+                self.report("File is now open.")
                 return open(filename)
             except (IOError, OSError):
-                cls.reporterror("Nonexistent or unreadable file.")
+                self.reporterror("Nonexistent or unreadable file.")
         elif mode == "w":
-            cls.report("Opening file " +
+            self.report("Opening file " +
                        filename + " for writing.")
             try:
                 f = open(filename,"w")
-                cls.report("File is now open.")
+                self.report("File is now open.")
                 return f
             except (IOError, OSError):
-                cls.reporterror("Unable to open file.")
+                self.reporterror("Unable to open file.")
         else:
-            cls.reporterror("Requested to open file in mode '" +
+            self.reporterror("Requested to open file in mode '" +
                             mode + "': no such mode available.")
 
-    def sound_bell(cls):
-        cls.console.bell()
+    def sound_bell(self):
+        self.console.bell()
 
